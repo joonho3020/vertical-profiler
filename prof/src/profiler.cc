87843c1bfcb8ee65ece63df29d444a33574926a8
joey0320 <joonho0320@gmail.com>
2023-12-08 11:18:01 -0800

    incorporate single step spike sim into prof

--- prof/src/profiler.cc
+++ prof/src/profiler.cc
@@ -7,11 +7,35 @@
 #include "objdump_parser.h"
 #include "profiler.h"
 
+#include <riscv/cfg.h>
+#include <riscv/debug_module.h>
+#include <riscv/devices.h>
+#include <riscv/log_file.h>
+#include <riscv/processor.h>
+#include <riscv/simif.h>
+#include <riscv/sim.h>
+#include <riscv/sim_lib.h>
 
-profiler_t::profiler_t(std::vector<std::string> objdump_paths) {
+
+profiler_t::profiler_t(std::vector<std::string> objdump_paths,
+      const cfg_t *cfg, bool halted,
+      std::vector<std::pair<reg_t, abstract_mem_t*>> mems,
+      std::vector<device_factory_t*> plugin_device_factories,
+      const std::vector<std::string>& args,
+      const debug_module_config_t &dm_config,
+      const char *log_path,
+      bool dtb_enabled,
+      const char *dtb_file,
+      bool socket_enabled,
+      FILE *cmd_file)
+{
   for (auto& p: objdump_paths) {
     objdumps.insert({p, new ObjdumpParser(p)});
   }
+
+  spike = new sim_lib_t(cfg, halted, mems, plugin_device_factories, args,
+                        dm_config, log_path, dtb_enabled, dtb_file,
+                        socket_enabled, cmd_file);
 }
 
 profiler_t::~profiler_t() {
@@ -22,4 +46,13 @@ profiler_t::~profiler_t() {
 }
 
 std::string profiler_t::find_user_space_binary(addr_t inst_va) {
+}
+
+int profiler_t::run() {
+  spike->init();
+  while (spike->target_running()) {
+    spike->advance(1);
+  }
+  auto rc = spike->stop_sim();
+  return rc;
 }
--- prof/src/profiler.h
+++ prof/src/profiler.h
@@ -1,20 +1,42 @@
 #ifndef __PROFILER_H__
 #define __PROFILER_H__
 
-
 #include <string>
 #include <vector>
 #include <map>
+#include <riscv/cfg.h>
+#include <riscv/debug_module.h>
+#include <riscv/devices.h>
+#include <riscv/log_file.h>
+#include <riscv/processor.h>
+#include <riscv/simif.h>
+#include <riscv/sim.h>
+#include <riscv/sim_lib.h>
 
 #include "types.h"
 #include "objdump_parser.h"
 
 class profiler_t {
 public:
-  profiler_t::profiler_t(std::vector<std::string> objdump_paths);
-  profiler_t::~profiler_t();
+  profiler_t(std::vector<std::string> objdump_paths,
+      const cfg_t *cfg, bool halted,
+      std::vector<std::pair<reg_t, abstract_mem_t*>> mems,
+      std::vector<device_factory_t*> plugin_device_factories,
+      const std::vector<std::string>& args,
+      const debug_module_config_t &dm_config,
+      const char *log_path,
+      bool dtb_enabled,
+      const char *dtb_file,
+      bool socket_enabled,
+      FILE *cmd_file);
+
+  ~profiler_t();
 
   std::string find_user_space_binary(addr_t inst_va);
+
+  int run();
+
+  sim_lib_t* spike;
 
 private:
   std::map<std::string, ObjdumpParser*> objdumps;
