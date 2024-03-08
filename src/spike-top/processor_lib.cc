7976492526bc980e3f48f9067f891ee5245be892
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-03-08 00:42:59 -0800

    Use mcycle + instret as the timing proxy for spike traces

--- src/spike-top/processor_lib.cc
+++ src/spike-top/processor_lib.cc
@@ -291,7 +291,7 @@ void processor_lib_t::step(size_t n) {
           insn_fetch_t fetch = mmu->load_insn(pc);
           if (debug && !state.serialized)
             disasm(fetch.insn);
-          trace.push_back({pc, get_asid(), get_mcycle()});
+          trace.push_back({pc, get_asid(), get_mcycle() + instret});
           pc = execute_insn_logged(this, pc, fetch);
           advance_pc();
         }
@@ -301,7 +301,7 @@ void processor_lib_t::step(size_t n) {
         // Main simulation loop, fast path.
         for (auto ic_entry = _mmu->access_icache(pc); ; ) {
           auto fetch = ic_entry->data;
-          trace.push_back({pc, get_asid(), get_mcycle()});
+          trace.push_back({pc, get_asid(), get_mcycle() + instret});
           pc = execute_insn_fast(this, pc, fetch);
           ic_entry = ic_entry->next;
           if (unlikely(ic_entry->tag != pc))
